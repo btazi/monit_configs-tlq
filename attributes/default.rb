47873410039acc47b94b1ab5c6922d9ccd6d5374
Badr Tazi <tasibadr@gmail.com>
2016-12-30 14:35:08 +0000

    slack alerts and elasticsearch monit conf file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['monit_configs-tlq']['postgres']['pidfile'] = '/var/run/postgresql/9.5-main.pid'
 # for the community cookbook use '/var/lib/mongo/mongod.lock'
 default['monit_configs-tlq']['mongo']['pidfile'] = '/var/run/mongo/mongo.pid'
+default['monit_configs-tlq']['ruby_path'] = "/usr/local/rbenv/versions/2.3.1/bin/ruby"
